d51a68affd26efe39645842a8389b2ee7658d77d
BugHunter-X <askzenai@gmail.com>
2024-12-27 19:20:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a test to reproduce a Next.js 15 bug.</p>
+    </div>
+  );
+}
+```
